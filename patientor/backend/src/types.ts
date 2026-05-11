cb077e86f87075bfbff938d1b8a909cad7b9c5d2
nicolasgavriil <219146079+nicolasgavriil@users.noreply.github.com>
2026-05-11 10:14:51 +0000

    Chapter 6 Exercise 25

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -7,18 +7,6 @@ export type Diagnosis = {
   latin?: string;
 };
 
-export const Gender = {
-  Male: "male",
-  Female: "female",
-  Other: "other",
-} as const;
-
-export type Gender = (typeof Gender)[keyof typeof Gender];
-
-export interface Entry {
-  id: string;
-}
-
 export type Patient = {
   id: string;
   name: string;
@@ -32,3 +20,56 @@ export type Patient = {
 export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
 
 export type NewPatient = z.infer<typeof NewPatientSchema>;
+
+export const Gender = {
+  Male: "male",
+  Female: "female",
+  Other: "other",
+} as const;
+
+export type Gender = (typeof Gender)[keyof typeof Gender];
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+const HealthCheckRating = {
+  Healthy: 0,
+  LowRisk: 1,
+  HighRisk: 2,
+  CriticalRisk: 3,
+} as const;
+
+type HealthCheckRating =
+  (typeof HealthCheckRating)[keyof typeof HealthCheckRating];
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
